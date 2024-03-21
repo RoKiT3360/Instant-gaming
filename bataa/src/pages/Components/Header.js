a32a92fae062223938186ce853be25086393c11f
RoKiT3360 <batamgalan3360@gmail.com>
2024-03-21 09:22:13 +0800

    bataa

---
+++ bataa/src/pages/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <div className="w-full h-[10vh] bg-transparent flex justify-between">
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+        </div>
+    )
+}
